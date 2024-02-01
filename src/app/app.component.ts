7c5a66ee2e5b2b2b557f11cac91895c9a5cc1496
tiagoforward <devtiagoforward@gmail.com>
2024-02-01 11:25:14 -0300

    criando e estruturando big card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
-import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
-import { BigCardComponent } from "./components/big-card/big-card.component";
-import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent, BigCardComponent, SmallCardComponent]
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [SmallCardComponent, BigCardComponent, MenuTitleComponent, MenuBarComponent]
+})
+export class HomeComponent {
+
+}
